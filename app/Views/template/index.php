903596cbb7991becdbf8aacab93c7bf50bb238b8
unknown <andrirahmat6937@gmail.com>
2024-01-14 14:03:59 +0700

    membuat menu blank page template

--- app/Views/template/index.php
+++ app/Views/template/index.php
@@ -343,6 +343,7 @@
                     <div class="container-xxl flex-grow-1 container-p-y">
                         <div class="row">
 
+                            <h1>Blank Page</h1>
 
                         </div>
 
